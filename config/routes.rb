3c22c4a0b07dca3a17d6633a980cc7ac04f6d7a3
clycrs <carolinelaycuras@hotmail.fr>
2020-11-18 16:36:42 +0100

    ok

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :restaurants do
+    resources :reviews, only: [ :new, :create ]
+  end
 end
+
+# rails generate model Restaurant name:string address:string phone_number:string category:string
+# rails db:migrate
+# rails generate model Review rating:integer content:string restaurant:references
+# rails db:migrate
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+7.times do
+  restaurant = Restaurant.new(
+    name: Faker::Beer.name,
+    address: Faker::Address.street_address,
+    category: ["chinese", "italian", "japanese", "french", "belgian"].sample,
+    phone_number: Faker::PhoneNumber.cell_phone
+  )
+  restaurant.save!
+end
